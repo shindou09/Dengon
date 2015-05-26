e5420c8a74f806fc5a00141bddc9769ce985598f
shindou09 <714226546@qq.com>
2015-05-26 11:50:31 +0800

    把jade模板改用ejs模板

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var serveStatic = require('serve-static');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -12,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(serveStatic('public', {index: false}));
 
 app.use('/', routes);
 app.use('/users', users);
